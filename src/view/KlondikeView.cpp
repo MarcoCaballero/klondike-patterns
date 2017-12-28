332f21294e5decd24e4ffef805192f5267827fef
MarcoCaballero <marco.caballero.d@gmail.com>
2017-12-28 19:29:42 +0100

    Finished ready to tag

--- src/view/KlondikeView.cpp
+++ src/view/KlondikeView.cpp
@@ -25,7 +25,6 @@ KlondikeView* KlondikeView::instance() {
 }
 
 std::string KlondikeView::getSingletonConfigure() {
-//	ifstream ifs("conf.txt");
 	ifstream ifs("src/view/conf.txt");
 	string line;
 	getline(ifs, line);
